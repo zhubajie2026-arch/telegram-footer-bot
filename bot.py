1e9c9e332dcfe867d34ac611666e214dae242ca1
zhubajie2026-arch <zhubajie2026@gmail.com>
2026-07-31 06:50:06 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,12 @@
+import os
 from telegram import Update
-from telegram.ext import (
-    Application,
-    MessageHandler,
-    ContextTypes,
-    filters,
-)
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
-# ===========================
-# 修改这里
-# ===========================
 
-BOT_TOKEN = "把这里改成你的Bot Token"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-CHANNEL_ID = "@你的频道用户名"
+CHANNEL_ID = "@dsj7000"
+
 
 FOOTER = """
 
@@ -20,46 +14,36 @@ FOOTER = """
 
 ⚠️ 温馨提示
 
-本频道所有信息均为免费发布，请自行甄别信息真实性，谨防诈骗。
+本频道所有信息均为免费发布，请注意甄别，谨防诈骗。
 
-🤝 初次交易建议先查看骗术曝光频道。
-
-🔍 骗术曝光：@jpzfzp
-🤖 投稿机器人：@xges_bot
-📰 华人头条：@hwhrtt
-📍 导航总群：@kaolaoerzhihuzheye
+🔍 骗术曝光 @jpzfzp
+🤖 投稿发布 @xges_bot
+📰 华人头条 @hwhrtt
+📍 导航总群 @kaolaoerzhihuzheye
 """
 
-# ===========================
 
-
-async def receive(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     msg = update.message
 
-    if msg is None:
+    if not msg:
         return
 
-    # 文字
     if msg.text:
-
         await context.bot.send_message(
             chat_id=CHANNEL_ID,
             text=msg.text + FOOTER
         )
 
-    # 图片
     elif msg.photo:
-
         await context.bot.send_photo(
             chat_id=CHANNEL_ID,
             photo=msg.photo[-1].file_id,
             caption=(msg.caption or "") + FOOTER
         )
 
-    # 视频
     elif msg.video:
-
         await context.bot.send_video(
             chat_id=CHANNEL_ID,
             video=msg.video.file_id,
@@ -69,13 +53,16 @@ async def receive(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = Application.builder().token(BOT_TOKEN).build()
 
+
 app.add_handler(
     MessageHandler(
         filters.ALL,
-        receive
+        forward
     )
 )
 
-print("机器人启动成功...")
+
+print("机器人启动成功")
+
 
 app.run_polling()
